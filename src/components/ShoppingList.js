4e79ce2da6fe75aae02c26e66cbe221fd8b496a4
RiayaEdy <edywidowaty@gmail.com>
2023-03-25 18:09:15 +1100

    changing dark-light mode and handle filter

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterEle, setFilterEle] = useState("All")
+  function handleFilter(event) {
+    setFilterEle(event.target.value)
+  }
+    const itemsFiltered=items.filter((item)=>{
+      if (filterEle === "All") {
+        return true;
+      }
+      return item.category === filterEle
+    })
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={handleFilter}>
+          <option value="All" >Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsFiltered.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
